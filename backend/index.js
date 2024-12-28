038990ade6af0ffceb0817186584cce88a348a55
kartiknarwal <kartiknarwal38@gmail.com>
2024-12-28 19:17:48 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,21 @@ const app = express();
 
 const server = http.createServer(app);
 
+const url = `https://render-hosting-se2b.onrender.com`;
+const interval =30000;
+
+function reloadWebsite(){
+  axios
+     .get(url)
+     .then((response)=>{
+        console.log("website reloaded");
+     })
+     .catch((error)=>{
+      console.error(`Error: ${error.message}`);
+     });
+}
+
+setInterval(reloadWebsite,interval);
 const io = new Server(server, {
   cors: {
     origin: "*",
