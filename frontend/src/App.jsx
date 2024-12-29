147ed9992c0a02ba2b7dceca6ea1e8513b6d520f
kartiknarwal <124709774+kartiknarwal@users.noreply.github.com>
2024-12-29 08:07:25 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import io from "socket.io-client";
 import Editor from "@monaco-editor/react";
 
-const socket = io("https://realtime-code-editor-2-3kx0.onrender.com");
+const socket = io("https://realtime-code-editor-3-zw8e.onrender.com");
 
 const App = () => {
   const [joined, setJoined] = useState(false);
